1a5a0a547e6e9d85ba30b434d4d8ba4caa831ed8
KyDat DGW <kydat9446@gmail.com>
2026-06-19 08:37:25 +0700

    Xóa mockdata

--- DoAn_QLTTT/Program.cs
+++ DoAn_QLTTT/Program.cs
@@ -1,7 +1,6 @@
 using DoAn_QLTTT.Data;
 using DoAn_QLTTT.Repositories;
 using DoAn_QLTTT.Repositories.Dapper;
-using DoAn_QLTTT.Repositories.Mock;
 using DoAn_QLTTT.Services;
 using Dapper;
 using Microsoft.AspNetCore.DataProtection;
@@ -22,41 +21,23 @@ builder.Services.AddDataProtection()
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSingleton<MockDataService>();
 builder.Services.AddScoped<DapperContext>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 builder.Services.AddScoped<IHoaDonService, HoaDonService>();
 builder.Services.AddScoped<INhacNoService, NhacNoService>();
 builder.Services.AddScoped<ISqlScriptReader, SqlScriptReader>();
 
-var dataProvider = builder.Configuration["DataProvider"] ?? "Mock";
-if (dataProvider.Equals("SqlServer", StringComparison.OrdinalIgnoreCase)
-    || dataProvider.Equals("Dapper", StringComparison.OrdinalIgnoreCase))
-{
-    builder.Services.AddScoped<ISqlDemoService, SqlDemoService>();
-    builder.Services.AddScoped<ILoaiPhongRepository, LoaiPhongDapperRepository>();
-    builder.Services.AddScoped<IPhongTroRepository, PhongTroDapperRepository>();
-    builder.Services.AddScoped<IKhachThueRepository, KhachThueDapperRepository>();
-    builder.Services.AddScoped<IHopDongRepository, HopDongDapperRepository>();
-    builder.Services.AddScoped<IDichVuRepository, DichVuDapperRepository>();
-    builder.Services.AddScoped<IChiSoDienNuocRepository, ChiSoDienNuocDapperRepository>();
-    builder.Services.AddScoped<IHoaDonRepository, HoaDonDapperRepository>();
-    builder.Services.AddScoped<IThanhToanRepository, ThanhToanDapperRepository>();
-    builder.Services.AddScoped<INguoiDungRepository, NguoiDungDapperRepository>();
-}
-else
-{
-    builder.Services.AddScoped<ISqlDemoService, MockSqlDemoService>();
-    builder.Services.AddScoped<ILoaiPhongRepository, LoaiPhongMockRepository>();
-    builder.Services.AddScoped<IPhongTroRepository, PhongTroMockRepository>();
-    builder.Services.AddScoped<IKhachThueRepository, KhachThueMockRepository>();
-    builder.Services.AddScoped<IHopDongRepository, HopDongMockRepository>();
-    builder.Services.AddScoped<IDichVuRepository, DichVuMockRepository>();
-    builder.Services.AddScoped<IChiSoDienNuocRepository, ChiSoDienNuocMockRepository>();
-    builder.Services.AddScoped<IHoaDonRepository, HoaDonMockRepository>();
-    builder.Services.AddScoped<IThanhToanRepository, ThanhToanMockRepository>();
-    builder.Services.AddScoped<INguoiDungRepository, NguoiDungMockRepository>();
-}
+// Register SQL Server/Dapper repositories unconditionally
+builder.Services.AddScoped<ISqlDemoService, SqlDemoService>();
+builder.Services.AddScoped<ILoaiPhongRepository, LoaiPhongDapperRepository>();
+builder.Services.AddScoped<IPhongTroRepository, PhongTroDapperRepository>();
+builder.Services.AddScoped<IKhachThueRepository, KhachThueDapperRepository>();
+builder.Services.AddScoped<IHopDongRepository, HopDongDapperRepository>();
+builder.Services.AddScoped<IDichVuRepository, DichVuDapperRepository>();
+builder.Services.AddScoped<IChiSoDienNuocRepository, ChiSoDienNuocDapperRepository>();
+builder.Services.AddScoped<IHoaDonRepository, HoaDonDapperRepository>();
+builder.Services.AddScoped<IThanhToanRepository, ThanhToanDapperRepository>();
+builder.Services.AddScoped<INguoiDungRepository, NguoiDungDapperRepository>();
 
 var app = builder.Build();
 
